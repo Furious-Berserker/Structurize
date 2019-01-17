9358277142fcf41e943a34138595ea6d176c673d
Furious-Berserker <raklov@icloud.com>
2019-01-17 15:41:05 +0300

    First commit

---
+++ app/src/main/java/com/svatoslavbulgakov/structurize/CheckActivity.java
@@ -0,0 +1,44 @@
+package com.svatoslavbulgakov.structurize;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class CheckActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check);
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        checkData();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+    }
+
+    private void checkData() {
+        if (currentUser != null){
+            Toast.makeText(this, "ZBS!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(CheckActivity.this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(CheckActivity.this, SignUpActivity.class);
+            startActivity(intent);
+        }
+        finish();
+    }
+}
